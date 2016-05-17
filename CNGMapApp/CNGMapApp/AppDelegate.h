57d39c0733db0a6f21a7a30fff08419d603d10a6
macbook pro <nebojasa@gmail.com>
2016-05-17 09:51:22 +0200

    start playing with maps

---
+++ CNGMapApp/CNGMapApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CNGMapApp
+//
+//  Created by Nebojsa Gujanicic on 17/05/2016.
+//  Copyright © 2016 Nebojsa Gujanicic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CNGMapApp/CNGMapApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CNGMapApp
+//
+//  Created by Nebojsa Gujanicic on 17/05/2016.
+//  Copyright © 2016 Nebojsa Gujanicic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@end
+
